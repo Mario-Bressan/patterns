191ca0b96c9c7d02a2fc0743518552b06f89b881
Mario Bressan <mario.bressan@hotmail.com>
2019-07-21 09:44:22 -0300

    Observer pattern implementing Observer of java.util.

---
+++ src/com/bressan/patterns/observer/ObserverTest.java
@@ -0,0 +1,16 @@
+package com.bressan.patterns.observer;
+
+public class ObserverTest {
+
+    public static void main(String[] args) {
+        NewsAgency observable = new NewsAgency();
+        NewsChannel observer = new NewsChannel();
+
+        observable.addObserver(observer);
+        observable.setNews("news");
+
+        System.out.println(observer.getNews());
+    }
+
+
+}
